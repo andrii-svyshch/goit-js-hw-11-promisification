4a70709dd29b2a74a188615c73258722353c74c5
andrii-svyshch <80419679+andrii-svyshch@users.noreply.github.com>
2021-09-14 17:30:20 +0300

    bugfix

--- js/task3.js
+++ js/task3.js
@@ -2,7 +2,7 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = transaction => {
+const makeTransaction = (transaction) => {
   const delay = randomIntegerFromInterval(200, 500);
 
   const promise = new Promise((resolve, reject) =>
@@ -10,20 +10,20 @@ const makeTransaction = transaction => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve({ id: transaction.id, time: delay });
       } else {
         reject(transaction.id);
       }
-    }, delay),
+    }, delay)
   );
   return promise;
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
-const logError = id => {
+const logError = (id) => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
